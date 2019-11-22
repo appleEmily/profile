59f85d4d6325ee243f39916b3f6ec505fab32918
appleEmily <emiri3128@gmail.com>
2019-11-22 23:39:01 +0900

    Initial Commit

---
+++ profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  profile
+//
+//  Created by 野崎絵未里 on 2019/11/22.
+//  Copyright © 2019 emily.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
